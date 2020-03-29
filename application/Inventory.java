45979a6932644b79d21e34e0c9054ffad4dbe6db
bdp1312 <bdp1312@gmail.com>
2020-03-29 01:20:41 -0500

    nights end

--- application/Inventory.java
+++ application/Inventory.java
@@ -5,15 +5,25 @@ import java.io.IOException;
 
 public class Inventory {
 	
-	
 	//creates new inventory file
 	boolean createInventory(String name){
-		//stub
-		return true;
+		String fileAddress = new String(name + ".txt");
+		try{
+			File newFile = new File(fileAddress);
+			  if (newFile.createNewFile()) {
+				  System.out.println("New Inventory File: " + newFile.getName());
+					return true;
+			  } else {
+				  System.out.println("File already exists.");	
+			  }
+		} catch (IOException e) {
+		      e.printStackTrace();
+		} 
+		return false;
 	}
 	
 	//adds new item to inventory file 
-	
+	void newInventory
 	
 	
 	//updates values of existing inventory item

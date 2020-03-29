fff889eb54e36c5c3e94782f0a7435cc061d57bf
bdp1312 <bdp1312@gmail.com>
2020-03-29 10:17:26 -0500

    mergeing two inventories

--- application/Inventory.java
+++ application/Inventory.java
@@ -1,12 +1,32 @@
 package application;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Inventory {
+	ItemEntry[] itemList;
+	File dataFile;
+	//constructor
+	public Inventory(String iName) throws FileNotFoundException{
+		this.dataFile = new File(iName+".txt");  
+		BufferedReader br = new BufferedReader(new FileReader(this.dataFile));
+		String rawText;
+		try {
+			while((rawText = br.readLine())!= null){
+				
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	
 	//creates new inventory file
-	boolean createInventory(String name){
+	public static boolean createInventory(String name){
 		String fileAddress = new String(name + ".txt");
 		try{
 			File newFile = new File(fileAddress);
@@ -22,8 +42,11 @@ public class Inventory {
 		return false;
 	}
 	
+	//opens inventory file and reads contents to memory
+	
+	
 	//adds new item to inventory file 
-	void newInventory
+	void newInventoryItem(){}
 	
 	
 	//updates values of existing inventory item
